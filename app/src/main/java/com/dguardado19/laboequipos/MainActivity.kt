3aecb7ac63b80e6a9e8b596623a026227d41c990
DGuardado19 <Piploc19>
2019-06-10 17:57:24 -0600

    .

--- app/src/main/java/com/dguardado19/laboequipos/MainActivity.kt
+++ app/src/main/java/com/dguardado19/laboequipos/MainActivity.kt
@@ -2,9 +2,13 @@ package com.dguardado19.laboequipos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dguardado19.laboequipos.viewModel.peliculasViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
